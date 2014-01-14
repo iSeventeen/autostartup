0ce0eacfc914cc7e9663a8457c9e40da42ed00a1
alex <alex@originatechia.com>
2014-01-14 11:45:14 +0800

    use camera

---
+++ src/com/android/activities/CameraSurfacePreview.java
@@ -0,0 +1,56 @@
+package com.android.activities;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraSurfacePreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraSurfacePreview(Context context) {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void takePicture(PictureCallback imageCallback) {
+        mCamera.takePicture(null, null, imageCallback);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = Camera.open();
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            Log.d("Dennis", "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        try {
+            mCamera.startPreview();
+        } catch (Exception e) {
+            Log.d("Dennis", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (null != mCamera) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+}
